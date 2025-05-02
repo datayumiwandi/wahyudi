5cc5141ad633145e9956cf67b2f13455e04eabba
datayumiwandi <datayumiwandi@gmail.com>
2025-05-02 18:54:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -26,7 +26,7 @@ const CONVERTER_URL = "https://api.foolvpn.me/convert";
 const DONATE_LINK = "https://trakteer.id/mayumiwandi/tip";
 const BAD_WORDS_LIST =
   "https://gist.githubusercontent.com/adierebel/a69396d79b787b84d89b45002cb37cd6/raw/6df5f8728b18699496ad588b3953931078ab9cf1/kata-kasar.txt";
-const PROXY_PER_PAGE = 24;
+const PROXY_PER_PAGE = 20;
 const WS_READY_STATE_OPEN = 1;
 const WS_READY_STATE_CLOSING = 2;
 const CORS_HEADER_OPTIONS = {
